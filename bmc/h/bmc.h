d0e717bdfb98163daf06ce33f03a825030d5cc3c
Stefan Lænner <slaenner@gmail.com>
2014-11-24 17:51:40 +0100

    Minor updates

--- bmc/h/bmc.h
+++ bmc/h/bmc.h
@@ -1,14 +1,14 @@
-
-
-
-/* Access from ARM Running Linux */
+#if !defined(BMC_H)
+#define BMC_H
 
 #define BCM2708_PERI_BASE        0x20000000
 #define GPIO_BASE                (BCM2708_PERI_BASE + 0x200000) /* GPIO controller */
 #define MAXTIMINGS 100
 
-#define AMP_R_CHANNEL 22
-#define AMP_L_CHANNEL 23
+#define PIN22 22
+#define PIN23 23
 
 void ControlPin(int pin, int state);
 void InitPin(int pin);
+
+#endif /* BMC_H */
